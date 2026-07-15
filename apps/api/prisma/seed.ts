455f2edfda1c6cece796394fbae32e9e5d4dc306
Kaushik Ghori <ghorikaushik0987@gmial.com>
2026-07-15 22:18:33 +0530

    refactor: remove unused seed files and related imports from the database seeding process
    - Deleted seed files for categories, brands, products, countries, and permissions.
    - Updated the seed.ts file to only include the seeding of roles, streamlining the database initialization process.

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -3,9 +3,6 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 import { seedRoles } from './seeds/role.seed';
-import { seedCategories } from './seeds/category.seed';
-import { seedBrands } from './seeds/brand.seed';
-import { seedProducts } from './seeds/product.seed';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -19,9 +16,6 @@ async function main() {
   console.log('🌱 Database Seeding Started...\n');
 
   await seedRoles(prisma);
-  await seedCategories(prisma);
-  await seedBrands(prisma);
-  await seedProducts(prisma);
 
   console.log('\n🎉 Database Seeding Completed');
 }
